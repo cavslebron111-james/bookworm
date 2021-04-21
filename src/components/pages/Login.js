0caa83ae1c1b8f08b350921af9861fe426035d65
cavslebron111-james <cavslebron111@gmail.com>
2021-04-21 20:20:03 +0700

    first commit

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,9 +1,12 @@
 import React,{Component} from 'react';
 import LoginForm from '../Forms/LoginForm';
-
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
+import {login} from "../../actions/auth";
  class Login extends Component {
     submit = (data) =>{
-    console.log(data);
+    this.props.login(data)
+    .then(()=> this.props.history.push("/"));
 
     };
     
@@ -20,4 +23,14 @@ import LoginForm from '../Forms/LoginForm';
     } 
 }
 
-export default Login;
+
+Login.propTypes = {
+    history: PropTypes.shape({
+        push: PropTypes.func.isRequired
+    }).isRequired,
+login: PropTypes.func.isRequired
+
+};
+
+
+export default connect(null,{login})(Login);
